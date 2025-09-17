9f654419631a06d76bafbd981852477bcbfc9551
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-09-17 09:47:29 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import Arvore.*;
+import Utils.File;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<String> nomes = File.leituraDosNomes("src/nomes");
+        Arvore arvoreGenealogica = new Arvore();
+        List<Pessoa> nomesNaoInseridos = new ArrayList<>();
+        for (int i = 0; i < (nomes.size() - 1); i++) {
+            Pessoa pai = new Pessoa();
+            Pessoa filho = new Pessoa();
+
+            pai.setNome(nomes.get(i));
+            filho.setNome(nomes.get(i+1));
+            filho.setPai(pai);
+            if(!arvoreGenealogica.inserir(pai, filho)) {
+                nomesNaoInseridos.add(pai);
+                nomesNaoInseridos.add(filho);
+            }
+        }
+        while(!nomesNaoInseridos.isEmpty()) {
+            for (int j = 0; j < nomesNaoInseridos.size(); j++) {
+                if(arvoreGenealogica.inserir(nomesNaoInseridos.get(j), nomesNaoInseridos.get(j+1))) {
+                    nomesNaoInseridos.remove(j);
+                    nomesNaoInseridos.remove(j);
+                    j -= 2;
+                }
+            }
+        }
+
+        arvoreGenealogica.mostrarArvoreVisualmente();
     }
 }
