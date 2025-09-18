cb70fbe097b1df69e8d07014d1227c478e340922
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-09-17 21:01:36 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,81 @@ import Utils.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         List<String> nomes = File.leituraDosNomes("src/nomes");
         Arvore arvoreGenealogica = new Arvore();
         List<Pessoa> nomesNaoInseridos = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
         for (int i = 0; i < (nomes.size() - 1); i++) {
             Pessoa pai = new Pessoa();
             Pessoa filho = new Pessoa();
 
-            pai.setNome(nomes.get(i));
-            filho.setNome(nomes.get(i+1));
+            filho.setNome(nomes.get(i));
+            pai.setNome(nomes.get(i + 1));
             filho.setPai(pai);
-            if(!arvoreGenealogica.inserir(pai, filho)) {
-                nomesNaoInseridos.add(pai);
+
+            // System.out.println("\nFilho: "+ nomes.get(i));
+            // System.out.println("\nPai: "+ nomes.get(i+1));
+            if (!arvoreGenealogica.inserir(filho, pai)) {
                 nomesNaoInseridos.add(filho);
+                nomesNaoInseridos.add(pai);
+                // System.out.println("Lista dos nomes não inseridos: " + nomesNaoInseridos);
             }
+            i++;
         }
-        while(!nomesNaoInseridos.isEmpty()) {
+        while (!nomesNaoInseridos.isEmpty()) {
             for (int j = 0; j < nomesNaoInseridos.size(); j++) {
-                if(arvoreGenealogica.inserir(nomesNaoInseridos.get(j), nomesNaoInseridos.get(j+1))) {
+                if (arvoreGenealogica.inserir(nomesNaoInseridos.get(j), nomesNaoInseridos.get(j + 1))) {
                     nomesNaoInseridos.remove(j);
                     nomesNaoInseridos.remove(j);
-                    j -= 2;
+                    j--;
                 }
             }
         }
 
-        arvoreGenealogica.mostrarArvoreVisualmente();
+        while (true) {
+            System.out.println("\n----------ARVORE GENEALOGICA----------\n");
+            System.out.println("1. Mostrar a árvore\n");
+            System.out.println("2. Buscar parentesco\n");
+            System.out.println("3. Sair do programa\n");
+
+            int escolha = scanner.nextInt();
+            scanner.nextLine();
+
+            if (escolha == 1) {
+                arvoreGenealogica.imprimirArvore(arvoreGenealogica.getRaiz(), "");
+            }
+
+            else if (escolha == 2) {
+                System.out.print("\nDigite o primeiro nome (nome.sobrenome): ");
+                String nome1 = scanner.nextLine();
+                Pessoa resultado1 = arvoreGenealogica.buscar(arvoreGenealogica.getRaiz(), nome1);
+                if (resultado1 == null) {
+                    System.out.println("ERRO: " + nome1 + " não existe na árvore!");
+                    continue;
+                }
+                System.out.print("\nDigite o segundo nome (nome.sobrenome): ");
+                String nome2 = scanner.nextLine();
+                Pessoa resultado2 = arvoreGenealogica.buscar(arvoreGenealogica.getRaiz(), nome2);
+                if (resultado2 == null) {
+                    System.out.println("ERRO: " + nome2 + " não existe na árvore!");
+                    continue;
+                }
+
+                // Espaço para fazer a funcao de buscar parentesco
+            }
+
+            else if (escolha == 3) {
+                System.out.println("\nFim do programa!\n");
+                break;
+            }
+
+        }
+
+        scanner.close();
     }
 }
